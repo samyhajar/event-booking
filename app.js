066db7aa3e5acf18754dd2a3533fe2a9056593d6
Samy <samyhajar@Samys-MacBook-Pro.local>
2022-10-24 17:52:27 +0200

    finished mongoDb/backend logic

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const { restart } = require('nodemon');
 const graphqlHttp = require('express-graphql').graphqlHTTP;
 const mongoose = require('mongoose');
-
+const isAuth = require('./middleware/is-auth');
 const graphqlSchema = require('./graphql/schema/index');
 const graphqlResolvers = require('./graphql/resolvers/index');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(isAuth);
 
 app.use(
   '/graphql',
--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,5 +1,5 @@
 const Event = require('../../models/event');
-
+const User = require('../../models/user');
 const { transformEvent } = require('./merge');
 
 module.exports = {
@@ -13,7 +13,10 @@ module.exports = {
       throw err;
     }
   },
-  createEvent: async (args) => {
+  createEvent: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error('Unauthenticated');
+    }
     const event = new Event({
       title: args.eventInput.title,
       description: args.eventInput.description,
