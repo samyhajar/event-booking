a158a8a4a26d791000024c456379f8d5d0e777f5
Samy <samyhajar@Samys-MacBook-Pro.local>
2022-10-27 11:27:10 +0200

    added Navbar (non responsive for now)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
+import './App.css';
 import AuthPage from '/Users/samyhajar/projects/event-booking/frontend/src/pages/Auth.js';
+import BookingsPage from '/Users/samyhajar/projects/event-booking/frontend/src/pages/Bookings.js';
+import EventsPage from '/Users/samyhajar/projects/event-booking/frontend/src/pages/Events.js';
 import React, { Component } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter className="App">
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          {/* <Route path="/" element={<Navigate to="/Auth" />} /> */}
-          <Route to="/auth" component={AuthPage} />
-          <Route path="/events" component={null} />
-          <Route path="/bookings" component={null} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} exact />
+              <Route path="/bookings" component={BookingsPage} exact />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
