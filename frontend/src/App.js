dabaad44a87330da6590a794c1070c83c2adc3f5
Samy <samyhajar@Samys-MacBook-Pro.local>
2022-10-27 10:40:41 +0200

    added frontend and react redirect and navigate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import AuthPage from '/Users/samyhajar/projects/event-booking/frontend/src/pages/Auth.js';
+import React, { Component } from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter className="App">
+        <Switch>
+          <Redirect from="/" to="/auth" exact />
+          {/* <Route path="/" element={<Navigate to="/Auth" />} /> */}
+          <Route to="/auth" component={AuthPage} />
+          <Route path="/events" component={null} />
+          <Route path="/bookings" component={null} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
